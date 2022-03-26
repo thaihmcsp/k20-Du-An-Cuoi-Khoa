8e519d4237efca2ffc30ca976db0641fa593575e
Your Name <you@example.com>
2022-03-26 22:00:19 +0700

    second

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -1,7 +1,18 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const path = require("path");
+const userModel = require("../models/userModel");
+const { checkForm, checkUser } = require("../middleWare/checkLogin");
 
-router.get('/', function(req,res){
-  res.json('okokoko')
-})
+router.get("/home", checkForm, (req, res) => {
+  res.render("user/home/home");
+});
 
-module.exports = router
+router.get("/register", checkUser, (req, res) => {
+  res.render("user/signUp/signUp");
+});
+
+router.get("/login", checkUser, (req, res) => {
+  res.render("user/signIn/signIn");
+});
+
+module.exports = router;

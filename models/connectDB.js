7f060ae50d0a0da2ae8e8ee8947c8f75d378b55c
Your Name <you@example.com>
2022-06-19 17:59:31 +0700

    update

--- models/connectDB.js
+++ models/connectDB.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-// mongoose.connect(
-//   `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.0kwab.mongodb.net/sang1?retryWrites=true&w=majority`
-// );
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.0kwab.mongodb.net/sang1?retryWrites=true&w=majority`
+);
 
-mongoose.connect("mongodb://localhost/k20-duan");
+// mongoose.connect("mongodb://localhost/k20-duan");
 
 module.exports = mongoose;
